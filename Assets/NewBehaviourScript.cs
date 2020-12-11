588e9dae8334c4643f2848c106b9bdfd96c08d58
嶋津恒彦 <simanezumi1989@gmail.com>
2020-12-11 22:29:25 +0900

    モデルのインポート

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /* 必要なモデル
+     　・床
+     　・壁
+    　 ・天井
+    　 ・ドア
+　     ・机
+    　 ・ソファ
+    　 ・宝箱x2
+    　 ・鍵
+    　 ・コイン
+    　 ・ヒントのイラスト
+    　 ・虫眼鏡イラスト
+    ---------
+       ・ヒントのタイル（赤/青/黄）
+
+     */
+}
