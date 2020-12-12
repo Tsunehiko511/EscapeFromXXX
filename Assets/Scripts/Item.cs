75b8b80e4d02da9fe752215772939f596c2040de
嶋津恒彦 <simanezumi1989@gmail.com>
2020-12-13 00:05:34 +0900

    アイテムの取得

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    // 種類を持っている
+    public enum Type
+    {
+        RedTile,
+        BlueTile,
+        YellowTile,
+        Key,
+        Coin,
+    }
+    public Type type;
+
+    // TODO:画像を持っている
+    
+}
