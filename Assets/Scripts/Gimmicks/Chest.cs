d6d4339a645af64ef61cca1034caaaf2a818facd
嶋津恒彦 <simanezumi1989@gmail.com>
2020-12-19 22:18:36 +0900

    宝箱のアニメーション

---
+++ Assets/Scripts/Gimmicks/Chest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    // Panelをクリックされたら
+    // アニメーションを再生
+    Animation anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animation>();
+        // OnClickObj();
+    }
+
+    public void OnClickObj()
+    {
+        Open();
+    }
+
+    void Open()
+    {
+        anim.Play();
+    }
+}
