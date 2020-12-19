e457e8a4960995f665088711c448500a06163cb8
嶋津恒彦 <simanezumi1989@gmail.com>
2020-12-19 20:36:50 +0900

    ズーム機能の実装

--- Assets/Scripts/Cameras/CameraManager.cs
+++ Assets/Scripts/Cameras/CameraManager.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // やりたいこと
-    // ・カメラの回転：特定の位置に移動する
+    // どこからでもアクセスできるようにする
+    public static CameraManager instance;
 
+    [SerializeField] GameObject backButton = default;
+    [SerializeField] GameObject leftButton = default;
+    [SerializeField] GameObject rightButton = default;
     [SerializeField] Transform[] cameraPositions;
+    Camera zoomCamera = default;
+    Camera mainCamera = default;
+
     int currentIndex = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Start()
     {
+        mainCamera = Camera.main;
         currentIndex = 0;
         SetCameraPosition(currentIndex);
+        backButton.SetActive(false);
     }
 
     public void TurnRight()
@@ -36,10 +49,30 @@ public class CameraManager : MonoBehaviour
         SetCameraPosition(currentIndex);
     }
 
+    public void OnBack()
+    {
+        backButton.SetActive(false);
+        // メインカメラに戻す = Zoomカメラを非表示&メインカメラを表示
+        this.zoomCamera.gameObject.SetActive(false);
+        // Camera.main.gameObject.SetActive(true); // NG
+        mainCamera.gameObject.SetActive(true);
+        leftButton.SetActive(true);
+        rightButton.SetActive(true);
+    }
+
     void SetCameraPosition(int index)
     {
         Camera.main.transform.position = cameraPositions[index].position;
         Camera.main.transform.rotation = cameraPositions[index].rotation;
+    }
+
+    public void SetZoomCamera(Camera zoomCamera)
+    {
+        this.zoomCamera = zoomCamera;
+        backButton.SetActive(true);
+        leftButton.SetActive(false);
+        rightButton.SetActive(false);
+        Camera.main.gameObject.SetActive(false);
     }
 
 }
---
+++ Assets/Scripts/Cameras/ZoomCameraSetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCameraSetter : MonoBehaviour
+{
+    [SerializeField] Camera zoomCamera = default;
+
+    // クリックされたら、ズーム用にカメラに切り替える
+    public void OnClickObj()
+    {
+        // CameraManagerに使いたいカメラを渡す
+        Debug.Log("カメラをズーム");
+        zoomCamera.gameObject.SetActive(true);
+        CameraManager.instance.SetZoomCamera(zoomCamera);
+    }
+}
